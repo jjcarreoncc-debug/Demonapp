a472eb1679a5d0c4e53779b0d15ce5e534c8e248
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-04-29 10:56:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -212,25 +212,22 @@ st.plotly_chart(fig_margen, use_container_width=True)
     # PRODUCTO
     # -------------------------
  if "Producto" in df.columns:
-        st.markdown("---")
-        st.subheader("📦 Ventas por Producto")
+    st.markdown("---")
+    st.subheader("📦 Ventas por Producto")
 
-        ventas_prod = (
-            df.groupby("Producto")["Ventas"]
-            .sum()
-            .sort_values(ascending=False)
-            .reset_index()
-        )
+    ventas_prod = (
+        df.groupby("Producto")["Ventas"]
+        .sum()
+        .sort_values(ascending=False)
+        .reset_index()
+    )
 
-        fig_prod = px.bar(
-            ventas_prod,
-            x="Producto",
-            y="Ventas",
-            color="Ventas"
-        )
-
-        st.plotly_chart(fig_prod, use_container_width=True)
-
+    fig_prod = px.bar(
+        ventas_prod,
+        x="Producto",
+        y="Ventas",
+        color="Ventas"
+    )
    
     # -------------------------
     # CLIENTES
