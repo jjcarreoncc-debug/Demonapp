de47dcc1a9f5c9afeb4cd853dab4d3ca089ab727
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-04-30 18:29:17 -0500

    Fix conditional statement for 'detalle' view

--- streamlit_app.py
+++ streamlit_app.py
@@ -420,7 +420,7 @@ else:
     # =========================
     # DETALLE
     # =========================
-    if st.session_state.vista == "detalle":
+   elif st.session_state.vista == "detalle":
 
     if st.button("⬅️ Volver"):
         st.session_state.vista = "principal"
