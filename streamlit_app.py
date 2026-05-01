9dd513562d3899fe5b40ce886dc396efbc66d536
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-01 02:09:49 -0500

    Fix indentation for authenticator login call

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=1
 )
 
-    name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login", "main")
 
 ######
 # AUTENTICACION
