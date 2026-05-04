06df4c739e40370c6f2b29baece26247102dc874
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-03 20:58:55 -0500

    Add logo image to Streamlit app layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,6 +93,12 @@ if authentication_status is None:
         img = Image.open("imagen7.png")
         st.image(img, width=2000)
     st.stop()
+# ------------------------
+# LOGO
+# ------------------------
+col1, col2, col3 = st.columns([1,2,1])
+with col2:
+    st.image("LOOGO-TIDS-CONSULTING (2).jpg", width=200)
 
 # ------------------------
 # FUNCIONES USUARIOS
