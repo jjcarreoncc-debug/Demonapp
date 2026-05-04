95a1c3fd31c2d57a859057bda528839889e748db
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-04 03:39:24 -0500

    Fix import statement for plotly.express

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as   
+import plotly.express as  px 
 import sqlite3
 import streamlit_authenticator as stauth
 from streamlit_authenticator import Hasher
