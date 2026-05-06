2c8dbe4a08e2470eb3d2da6876504dce0e4e404f
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-05 22:31:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -593,6 +593,7 @@ elif menu == "Dashboard":
     st.title("📊 Dashboard Principal")
 
 elif menu == "Inventarios":
+    inventarios_app() 
     st.title("📦 Inventarios")
 
 elif menu == "Mantenimiento":
