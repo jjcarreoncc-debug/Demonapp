61676d8ae7221cd18aec30263afa2ce5e722f0f2
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-06 16:21:47 -0500

    Change import from alertas to alertas_app

--- inventarios_app.py
+++ inventarios_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import base64
 import os
-from alertas import dashboard_criticos
+from alertas_app import dashboard_criticos_app
 
 # =========================
 # CSS
