00f8b7ab2750e3d21b0e447f228b4fc4d90abe88
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-05 22:13:28 -0500

    Fix indentation in inventarios_app function

--- inventarios_app.py
+++ inventarios_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import pandas as pd
 def inventarios_app():
-st.write("ARCHIVOS:", os.listdir())    
+    st.write("ARCHIVOS:", os.listdir())    
 #st.title("📦 Inventarios OK")
     st.title("📦 Inventarios")
 
