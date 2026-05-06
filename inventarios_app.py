a75eed51d6b8aafe795e778cc052f8ddebf643c6
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-06 10:07:56 -0500

    Fix column gap in inventory view

--- inventarios_app.py
+++ inventarios_app.py
@@ -81,7 +81,7 @@ def inventarios_app():
         st.session_state.inv_vista = "menu"
 
     if st.session_state.inv_vista == "menu":
-
+        c1, c2, c3, c4, c5 = st.columns(5, gap="large")  
         c1, c2, c3, c4, c5 = st.columns(5)
 
         if c1.button("General"):
