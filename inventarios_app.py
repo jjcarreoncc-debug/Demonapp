0652b458af57dba03a5302e936f0d308a25fe2c4
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-06 14:42:18 -0500

    Refactor card_kpi function to use st.markdown

--- inventarios_app.py
+++ inventarios_app.py
@@ -53,38 +53,38 @@ def set_bg():
     """, unsafe_allow_html=True)
 def card_kpi(titulo, valor, color="#1f77b4"):
 
-    html = f"""
-    <div style="
-        background-color:#f5f7fa;
-        padding:20px;
-        border-radius:15px;
-        border-left:8px solid {color};
-        box-shadow:2px 2px 10px rgba(0,0,0,0.1);
-        margin-bottom:15px;
-    ">
-
+    st.markdown(
+        f"""
         <div style="
-            font-size:18px;
-            font-weight:600;
-            color:#2c3e50;
+            background-color:#f5f7fa;
+            padding:20px;
+            border-radius:15px;
+            border-left:8px solid {color};
+            box-shadow:2px 2px 10px rgba(0,0,0,0.1);
+            margin-bottom:15px;
         ">
-            {titulo}
+
+            <div style="
+                font-size:18px;
+                font-weight:600;
+                color:#2c3e50;
+            ">
+                {titulo}
+            </div>
+
+            <div style="
+                font-size:30px;
+                font-weight:700;
+                color:{color};
+                margin-top:10px;
+            ">
+                {valor}
+            </div>
+
         </div>
-
-        <div style="
-            font-size:30px;
-            font-weight:700;
-            color:{color};
-            margin-top:10px;
-        ">
-            {valor}
-        </div>
-
-    </div>
-    """
-
-    st.markdown(html, unsafe_allow_html=True)
-
+        """,
+        unsafe_allow_html=True
+    )
 # =========================
 # PROCESO
 # =========================
