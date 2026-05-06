f0555fffdff001b63633af5c90dcf72ee8a7e79f
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-06 02:23:12 -0500

    Comment out debug output for data_ready
    Comment out debug statement for data_ready.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.set_page_config(page_title="Dashboard Ejecutivo", layout="wide")
 # =========================
 # ESTADO GLOBAL
 # =========================
-st.write("DEBUG data_ready:", st.session_state.data_ready)
+#st.write("DEBUG data_ready:", st.session_state.data_ready)
 if "menu" not in st.session_state:
     st.session_state.menu = "📂 Carga"
 
