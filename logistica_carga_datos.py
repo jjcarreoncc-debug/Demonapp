91efe5090d69d94a98d62abf8252d7dcb9fed76d
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-07 17:23:41 -0500

    Update logistica_carga_datos.py

--- logistica_carga_datos.py
+++ logistica_carga_datos.py
@@ -1,1 +1,58 @@
+import pandas as pd
+import streamlit as st
 
+
+# =========================
+# CARGA AUTOMÁTICA LOGÍSTICA
+# =========================
+def cargar_datos_logistica():
+
+    try:
+
+        transito = pd.read_excel(
+            "data/logistica/transito.xlsx"
+        )
+
+        bodegas = pd.read_excel(
+            "data/logistica/bodegas.xlsx"
+        )
+
+        transportistas = pd.read_excel(
+            "data/logistica/transportistas.xlsx"
+        )
+
+        rutas = pd.read_excel(
+            "data/logistica/rutas.xlsx"
+        )
+
+        recepcion = pd.read_excel(
+            "data/logistica/recepcion.xlsx"
+        )
+
+        despachos = pd.read_excel(
+            "data/logistica/despachos.xlsx"
+        )
+
+        return (
+            transito,
+            bodegas,
+            transportistas,
+            rutas,
+            recepcion,
+            despachos
+        )
+
+    except Exception as e:
+
+        st.error("❌ Error cargando archivos automáticos.")
+
+        st.exception(e)
+
+        return (
+            None,
+            None,
+            None,
+            None,
+            None,
+            None
+        )
