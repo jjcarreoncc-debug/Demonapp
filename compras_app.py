b2270e0e6c03f93fdd356d5173b3ab6391fe79e4
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-06 23:35:28 -0500

    Update compras_app.py

--- compras_app.py
+++ compras_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from ui_components import card_kp
+from ui_components import card_kpi
 
 ## =========================
 # CSS COMPRAS
