33edb8eec0ecb808682c8237107bb6511bec8061
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-07 22:07:00 -0500

    Update wms_carga_datos.py

--- wms_carga_datos.py
+++ wms_carga_datos.py
@@ -1,1 +1,39 @@
 
+import pandas as pd
+import streamlit as st
+
+
+def cargar_datos_wms():
+
+    try:
+        inventario = pd.read_excel("data/wms/inventario_wms.xlsx")
+        ubicaciones = pd.read_excel("data/wms/ubicaciones.xlsx")
+        entradas = pd.read_excel("data/wms/entradas_wms.xlsx")
+        salidas = pd.read_excel("data/wms/salidas_wms.xlsx")
+        picking = pd.read_excel("data/wms/picking.xlsx")
+        packing = pd.read_excel("data/wms/packing.xlsx")
+        movimientos = pd.read_excel("data/wms/movimientos_wms.xlsx")
+
+        return (
+            inventario,
+            ubicaciones,
+            entradas,
+            salidas,
+            picking,
+            packing,
+            movimientos
+        )
+
+    except Exception as e:
+        st.error("❌ Error cargando archivos automáticos de WMS.")
+        st.exception(e)
+
+        return (
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            None
+        )
