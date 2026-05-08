861f4f0ce586f6edcf29459d42be8e6d36047459
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-08 15:43:00 -0500

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,1 +1,18 @@
 
+import streamlit as st
+
+st.set_page_config(
+    page_title="Prueba Logos",
+    layout="centered"
+)
+
+st.title("PRUEBA DE LOGOS")
+
+st.write("Logo Fondo")
+st.image("logofondo.png", width=500)
+
+st.write("Logo Empresa")
+st.image("LOOGO-TIDS-CONSULTING (2).jpg", width=250)
+
+st.write("Logo SIGEM")
+st.image("logo1.png", width=300)
