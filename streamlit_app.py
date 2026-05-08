e67d4ca29992e0114531ee3423e0221fb6195019
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-08 17:53:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ st.set_page_config(page_title="Dashbo ard Ejecutivo", layout="wide")
 from database import init_database
 from seed_data import seed_data
 from auth_app import login_app, logout_app
+from ui_components import card_kpi, mostrar_logos
+
 
 
 
