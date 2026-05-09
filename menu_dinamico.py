30b8100a6211c0a697341ed58087ea3fa72cd119
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-09 09:34:31 -0500

    Update menu_dinamico.py

--- menu_dinamico.py
+++ menu_dinamico.py
@@ -7,33 +7,39 @@ from database import get_connection
 def sidebar_dinamico():
 
     conn = get_connection()
-
-    menu_df = pd.read_sql_query(
-        """
-        SELECT DISTINCT
-            m.nombre_modulo,
-            m.icono,
-            m.ruta,
-            m.orden_menu
-        FROM permisos_roles pr
-        INNER JOIN roles r
-            ON pr.id_rol = r.id_rol
-        INNER JOIN modulos m
-            ON pr.id_modulo = m.id_modulo
-        WHERE r.nombre_rol = ?
-        AND pr.puede_ver = 1
-        AND m.activo = 1
-        ORDER BY m.orden_menu
-        """,
-        conn,
-        params=(st.session_state.rol,)
-        st.sidebar.write("DEBUG rol:", st.session_state.rol)
-        st.sidebar.write("DEBUG registros query:", len(menu_df))
-
-    )
-
+    #
+    
     conn.close()
     
+    menu_df = pd.read_sql_query(
+    """
+    SELECT DISTINCT
+        m.nombre_modulo,
+        m.icono,
+        m.ruta,
+        m.orden_menu
+    FROM permisos_roles pr
+    INNER JOIN roles r
+        ON pr.id_rol = r.id_rol
+    INNER JOIN modulos m
+        ON pr.id_modulo = m.id_modulo
+    WHERE r.nombre_rol = ?
+    AND pr.puede_ver = 1
+    AND m.activo = 1
+    ORDER BY m.orden_menu
+    """,
+    conn,
+    params=(st.session_state.rol,)
+)
+
+st.sidebar.write("DEBUG rol:", st.session_state.rol)
+st.sidebar.write("DEBUG registros query:", len(menu_df))
+st.sidebar.dataframe(
+    menu_df,
+    use_container_width=True
+)
+
+st.stop()
     menu_df = pd.read_sql_query(
     """
     SELECT DISTINCT
