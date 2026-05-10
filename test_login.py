9ae3d0ffc564cb1723d04fe568b54bffd9cd624d
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-09 22:59:37 -0500

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,74 +1,17 @@
 import sqlite3
-
-# ==========================================
-# CONEXIÓN
-# ==========================================
+import pandas as pd
 
 conn = sqlite3.connect("sigem.db")
 
-cursor = conn.cursor()
-
 # ==========================================
-# CREAR USUARIO ADMIN
+# VER ESTRUCTURA TABLA USUARIOS
 # ==========================================
 
-cursor.execute(
-    """
-    INSERT INTO usuarios (
-        usuario,
-        password,
-        nombre_completo,
-        rol,
-        activo
-    )
-    VALUES (?, ?, ?, ?, ?)
-    """,
-    (
-        "admin",
-        "admin123",
-        "Administrador General",
-        "ALL",
-        1
-    )
+estructura = pd.read_sql_query(
+    "PRAGMA table_info(usuarios)",
+    conn
 )
 
-conn.commit()
-
-# ==========================================
-# ASIGNAR TODOS LOS MÓDULOS
-# ==========================================
-
-cursor.execute(
-    """
-    SELECT id_modulo
-    FROM modulos
-    """
-)
-
-modulos = cursor.fetchall()
-
-for modulo in modulos:
-
-    id_modulo = modulo[0]
-
-    cursor.execute(
-        """
-        INSERT INTO permisos_roles (
-            id_rol,
-            id_modulo,
-            puede_ver
-        )
-        VALUES (?, ?, ?)
-        """,
-        (
-            1,
-            id_modulo,
-            1
-        )
-    )
-
-conn.commit()
+print(estructura)
 
 conn.close()
-
-print("ADMIN CREADO CORRECTAMENTE")
