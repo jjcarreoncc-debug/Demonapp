77fc92cd1bf555c22f7e1e515715295cda60b8be
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-10 17:18:05 -0500

    Update test_login.py to show database table names
    Refactored the test_login.py script to display table names from the database instead of column information.

--- test_login.py
+++ test_login.py
@@ -1,13 +1,9 @@
-import streamlit as st
-import sqlite3
-import pandas as pd
+tablas = conn.execute("""
+    SELECT name
+    FROM sqlite_master
+    WHERE type='table'
+""").fetchall()
 
-conn = sqlite3.connect("erp.db")
-
-st.subheader("Columnas usuarios")
-st.dataframe(pd.read_sql_query("PRAGMA table_info(usuarios)", conn))
-
-st.subheader("Columnas roles")
-st.dataframe(pd.read_sql_query("PRAGMA table_info(roles)", conn))
-
-conn.close()
+st.write("TABLAS EN ESTA BD:")
+for t in tablas:
+    st.write(dict(t))
