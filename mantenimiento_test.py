6aa9190731fe757515a1ff97aaebddb750ff1734
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-11 08:38:37 -0500

    Update mantenimiento_test.py

--- mantenimiento_test.py
+++ mantenimiento_test.py
@@ -1,1 +1,12 @@
 
+def simular_login():
+
+    st.session_state.autenticado = True
+
+    st.session_state.usuario = "JCERVANTES"
+
+    st.session_state.nombre = "JOSE JUANCERVANTES"
+
+    st.session_state.rol = 1
+
+    st.session_state.perfil = "ALL"
