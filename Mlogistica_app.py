4e84f3cd453d5c27d59293d3502452d199f8bee1
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-13 09:06:49 -0500

    Update Mlogistica_app.py

--- Mlogistica_app.py
+++ Mlogistica_app.py
@@ -8,29 +8,20 @@ def logistica_app():
 
     menu_logistica, submenu_logistica, opcion_logistica = sidebar_logistica()
 
+    if opcion_logistica is None:
+        menu_logistica = "📦 Embarques"
+        submenu_logistica = "Embarques"
+        opcion_logistica = "➕ Alta embarque"
+
     st.title("🚚 Logística")
 
     st.divider()
 
-menu_logistica, submenu_logistica, opcion_logistica = sidebar_logistica()
+    if opcion_logistica == "➕ Alta embarque":
 
-if opcion_logistica is None:
-    opcion_logistica = "➕ Alta embarque"
+        alta_embarque_app()
 
-
-if opcion_logistica == "➕ Alta embarque":
-
-    alta_embarque_app()
-
-else:
-
-    st.subheader(opcion_logistica)
-
-    st.info("Módulo de Logística en construcción.")
-
-    st.write("Menú:", menu_logistica)
-    st.write("Submenú:", submenu_logistica)
-    st.write("Opción:", opcion_logistica)
+    else:
 
         st.subheader(opcion_logistica)
 
