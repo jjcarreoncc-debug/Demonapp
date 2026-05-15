1b9724911c4e45585ef7e97797576b29616e76c2
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-14 21:17:42 -0500

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -1,37 +1,49 @@
 import sqlite3
+import streamlit as st
+
 from sigem_db import get_db_path
 
-db_path = get_db_path("seguridad")
 
-conn = sqlite3.connect(db_path)
+def insertar_roles():
 
-cur = conn.cursor()
+    db_path = get_db_path("seguridad")
 
-roles = [
+    st.info(f"Base: {db_path}")
 
-    ("Admin", "Administrador del sistema"),
-    ("Gerencia", "Gerencia general"),
-    ("Compras", "Modulo compras"),
-    ("Logistica", "Modulo logistica"),
-    ("WMS", "Modulo almacen WMS"),
-    ("Consulta", "Solo consulta")
+    conn = sqlite3.connect(db_path)
 
-]
+    cur = conn.cursor()
 
-for nombre_rol, descripcion in roles:
+    roles = [
 
-    cur.execute("""
-        INSERT OR IGNORE INTO roles (
+        ("Admin", "Administrador del sistema"),
+        ("Gerencia", "Gerencia general"),
+        ("Compras", "Modulo compras"),
+        ("Logistica", "Modulo logistica"),
+        ("WMS", "Modulo almacen WMS"),
+        ("Consulta", "Solo consulta")
+
+    ]
+
+    for nombre_rol, descripcion in roles:
+
+        cur.execute("""
+            INSERT OR IGNORE INTO roles (
+                nombre_rol,
+                descripcion
+            )
+            VALUES (?, ?)
+        """, (
             nombre_rol,
             descripcion
-        )
-        VALUES (?, ?)
-    """, (
-        nombre_rol,
-        descripcion
-    ))
+        ))
 
-conn.commit()
-conn.close()
+        st.success(f"✅ Rol insertado: {nombre_rol}")
 
-print("✅ Roles insertados correctamente.")
+    conn.commit()
+    conn.close()
+
+    st.success("✅ Proceso terminado")
+
+
+insertar_roles()
