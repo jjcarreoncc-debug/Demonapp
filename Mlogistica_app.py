be8b967ec72613ea039a46bf840db5c7de8ad1fc
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-15 11:53:39 -0500

    Update Mlogistica_app.py

--- Mlogistica_app.py
+++ Mlogistica_app.py
@@ -99,15 +99,7 @@ def logistica_app():
         opcion_logistica
     )
     ##############################################3
-    if opcion_limpia != limpiar_opcion("🏠 Inicio"):
-
-        st.title("🚚 Logística")
-    
-        st.caption(
-            f"{menu_logistica} / {submenu_logistica} / {opcion_logistica}"
-        )
-    
-        st.divider()
+           
     #################################333
     if opcion_limpia == limpiar_opcion("🏠 Inicio"):
 
