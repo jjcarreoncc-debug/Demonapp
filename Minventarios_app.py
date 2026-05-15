f00fc7e3fbcf8b81e8eab565906dda246ae879ab
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-15 17:43:47 -0500

    Fix case sensitivity in 'embarque' option

--- Minventarios_app.py
+++ Minventarios_app.py
@@ -65,7 +65,7 @@ def inventarios_app():
     elif opcion_limpia == "consultar entradas compra":
         consulta_entradas_compras_app()
         
-    elif opcion_limpia == "embarque":
+    elif opcion_limpia == "Embarque":
         
         confirmacion_cancelacion_embarque_app()
 
