775292e020731667a71df49fe70e54d335616cfc
jjcarreoncc-debug <jjcarreoncc@gmail.com>
2026-05-15 19:57:02 -0500

    Update Minventarios_app.py

--- Minventarios_app.py
+++ Minventarios_app.py
@@ -43,7 +43,7 @@ def inicio_inventarios_app():
 
     try:
         st.image(
-            "logoinventarios.jpg",
+            "logoinventa.jpg",
             use_container_width=True
         )
     except Exception:
